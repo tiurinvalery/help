4b50e80fcdfb043171598d48e20396f8b3c8eb90
Tiurin Valery <tiurinvalery@gmail.com>
2018-03-05 16:02:26 +0200

    Fix problem with search

--- src/Score.java
+++ src/Score.java
@@ -2,7 +2,7 @@ public class Score {
     public static Integer getScore(String search, String source, String brand,String category) {
         FirstMethodCompare firstMethodCompare = new FirstMethodCompare();
         Integer resultValue = 0;
-        resultValue = firstMethodCompare.compareRusNames(search,source) - SortContainsBrand.sortContainsBrand(search, source, brand) - SortContainsCategory.getScore(search,source,category)
+        resultValue = firstMethodCompare.compareRusNames(search,source)  - SortContainsCategory.getScore(search,source,category)
                 +BonusScoreForNumber.getScore(search,source);
         return resultValue;
     }
